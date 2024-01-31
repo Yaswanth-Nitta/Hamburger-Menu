c9eef918181b7ff2fcf8d80011e6361baed45cf1
Yaswanth-Nitta <nxtwave@ip-10-2-0-9.ap-south-1.compute.internal>
2024-01-31 04:48:01 +0000

    Hamburger Menu

---
+++ src/components/Home/index.js
@@ -0,0 +1,18 @@
+import Header from '../Header'
+
+const Home = () => (
+  <div>
+    <Header />
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/home-sm-img.png"
+        alt="home"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+        alt="home"
+      />
+    </div>
+  </div>
+)
+export default Home
